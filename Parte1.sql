5e84ae386e7c3168c27fde6e0c3ecac88b28a8e5
Lucavinini <lucas.00000850855@unicap.br>
2025-09-09 12:39:50 -0300

    feat: Cria entidades

--- Parte1.sql
+++ Parte1.sql
@@ -6,3 +6,25 @@ USE sqlpratica;
 
 -- apenas para verificar onde estamos
 SELECT DATABASE();
+
+-- cria a tabela turma
+CREATE TABLE IF NOT EXISTS turma (
+    id INT PRIMARY KEY,
+    entrada DATE
+);
+
+-- cria a tabela professor
+CREATE TABLE IF NOT EXISTS professor (
+    id INT PRIMARY KEY,
+    nome VARCHAR(20)
+);
+
+-- cria a tabela aluno
+CREATE TABLE aluno (
+    id INT PRIMARY KEY,
+    nome VARCHAR(20),
+    idade INT,
+    id_turma INT,
+    curso VARCHAR(15),
+    FOREIGN KEY (id_turma) REFERENCES turma(id)
+);
