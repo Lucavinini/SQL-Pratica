36ca4456dd03971f8ee1e8097bdc2d0159e132b1
Lucavinini <lucas.00000850855@unicap.br>
2025-09-09 12:38:12 -0300

    feat: Insere comando de criação do banco de dados

---
+++ Parte1.sql
@@ -0,0 +1,8 @@
+-- cria o banco apenas se não existir
+CREATE DATABASE IF NOT EXISTS sqlpratica;
+
+-- seleciona o banco
+USE sqlpratica;
+
+-- apenas para verificar onde estamos
+SELECT DATABASE();
