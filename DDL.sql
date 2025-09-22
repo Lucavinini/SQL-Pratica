49e294a3264907f26d2fd1c38748c7c1b534c03a
Lucas Vinícius <148213832+Lucavinini@users.noreply.github.com>
2025-09-22 17:37:09 +0000

    feat: Seleciona o banco de dados

--- DDL.sql
+++ DDL.sql
@@ -4,3 +4,6 @@
 
 CREATE DATABASE IF NOT EXISTS Faculdade_DB; --(O "IF NOT EXISTS" serve para evitar erros caso o bd já exista)
 
+-- Passo 2: Seleciona o banco de dados para realizar operações sobre ele
+
+USE Faculdade_DB;
