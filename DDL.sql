8bec108e5775d41d7e524d1f06e7267f498bdd5b
Lucas Vinícius <148213832+Lucavinini@users.noreply.github.com>
2025-09-22 17:43:38 +0000

    style: adiciona seções 1 e 2 no script DDL

--- DDL.sql
+++ DDL.sql
@@ -1,5 +1,7 @@
 -- Aprendendo comandos do tipo DDL --
 
+-- Etapa 1 - (Inicialização)
+
 -- Passo 1: Cria o banco de dados que vai conter as tabelas
 
 CREATE DATABASE IF NOT EXISTS Faculdade_DB; --(O "IF NOT EXISTS" serve para evitar erros caso o bd já exista)
@@ -7,3 +9,5 @@ CREATE DATABASE IF NOT EXISTS Faculdade_DB; --(O "IF NOT EXISTS" serve para evit
 -- Passo 2: Seleciona o banco de dados para realizar operações sobre ele
 
 USE Faculdade_DB;
+
+-- Etapa 2 - (Comandos)
