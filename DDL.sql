527c325a586780a610e29992dc7a4323e512e1af
Lucas Vinícius <148213832+Lucavinini@users.noreply.github.com>
2025-09-22 17:35:06 +0000

    feat: Cria um banco de dados

---
+++ DDL.sql
@@ -0,0 +1,6 @@
+-- Aprendendo comandos do tipo DDL --
+
+-- Passo 1: Cria o banco de dados que vai conter as tabelas
+
+CREATE DATABASE IF NOT EXISTS Faculdade_DB; --(O "IF NOT EXISTS" serve para evitar erros caso o bd já exista)
+
